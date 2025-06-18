6e5e9302ba221d112f5a48579deb51a172ad26d3
AlessiaYChen <alessia.yanchen@gov.bc.ca>
2025-06-18 13:46:08 -0700

    update

--- Gcpe.MediaHub.API/Program.cs
+++ Gcpe.MediaHub.API/Program.cs
@@ -34,19 +34,7 @@ builder.Services.AddAuthentication("BasicAuthentication")
 
 builder.Services.AddAuthorization();
 
-// Add CORS
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:5173")
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
-});
-
-// Add CORS
+// Add CORS for localhost development
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(
